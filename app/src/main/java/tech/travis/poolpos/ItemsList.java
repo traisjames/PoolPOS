2de3855c8b7972e5d927c75965f5afd71e6c73d1
traisjames <traisjames@msn.com>
2015-02-07 14:54:45 -0600

    Initial commit

---
+++ app/src/main/java/tech/travis/poolpos/ItemsList.java
@@ -0,0 +1,13 @@
+package tech.travis.poolpos;
+
+/**
+ * Created by travis on 2/7/15.
+ */
+public class ItemsList {
+    public static final String[][] ENTRY = {
+            {"Daily", "Senior", "Infant"},{"4","2","0"}
+    };
+    public static final String[][] FOOD = {
+            {"Pizza", "Ice Creme", "Popcorn", "Pop", "SG Pop", "Candy", "Nachos", "Apple"},{"2","2","1","0.75"}
+    };
+}
---
+++ app/src/main/java/tech/travis/poolpos/MainActivity.java
@@ -0,0 +1,107 @@
+package tech.travis.poolpos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.sqrt;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createButtons();
+    }
+
+    public void createButtons()
+    {
+        LinearLayout layoutVertical = (LinearLayout) findViewById(R.id.ButtonLayout);
+        LinearLayout rowLayout=null;
+        int FoodSize = ItemsList.FOOD[0].length;
+        int x = (int) ceil(sqrt(FoodSize));
+        int y = (int) ceil(sqrt(FoodSize));
+        Button[][] buttons = new Button[y][x];
+        int count=x*y+1;
+        ViewGroup.LayoutParams param = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT,1);
+
+        //create buttons
+        for (int i = 0; i<y; i++)
+        {
+            if(count%x==1)
+            {
+                rowLayout = new LinearLayout(this);
+                rowLayout.setWeightSum(x);
+                layoutVertical.addView(rowLayout,param);
+                count=count-x;
+            }
+            for(int j=0;j<x;j++)
+            {
+                buttons[i][j]=new Button(this);
+                //Set button ID
+                buttons[i][j].setId(i*x+j);
+                //set text in buttons
+                if ((i*x+j) < FoodSize) {
+                    buttons[i][j].setText(ItemsList.FOOD[0][i * x + j] + " " + buttons[i][j].getId());
+                }
+                else
+                {
+                    buttons[i][j].setText("NA");
+                }
+                buttons[i][j].setTextSize(8);
+                buttons[i][j].setOnClickListener(new POSClickListener(i*x+j));
+                rowLayout.addView(buttons[i][j], param);
+
+            }
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Button onclick listener
+    class POSClickListener implements View.OnClickListener {
+        int buttonID;
+        public POSClickListener(int id) {
+            buttonID = id;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
+
+
